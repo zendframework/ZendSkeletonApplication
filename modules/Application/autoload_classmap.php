bf2802131336d687f3a773fbdb5c077948e28161
Evan Coury <me@evancoury.com>
2011-11-08 07:19:19 -0700

    Update classmap

--- modules/Application/autoload_classmap.php
+++ modules/Application/autoload_classmap.php
@@ -1,8 +1,7 @@
 <?php
-return array (
-  'Application\\View\\Listener' => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Application' . DIRECTORY_SEPARATOR . 'View' . DIRECTORY_SEPARATOR . 'Listener.php',
-  'Application\\View\\Helper\\Url' => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Application' . DIRECTORY_SEPARATOR . 'View' . DIRECTORY_SEPARATOR . 'Helper' . DIRECTORY_SEPARATOR . 'Url.php',
-  'Application\\Controller\\IndexController' => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Application' . DIRECTORY_SEPARATOR . 'Controller' . DIRECTORY_SEPARATOR . 'IndexController.php',
-  'Application\\Controller\\ErrorController' => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Application' . DIRECTORY_SEPARATOR . 'Controller' . DIRECTORY_SEPARATOR . 'ErrorController.php',
-  'Application\\Module' => __DIR__ . DIRECTORY_SEPARATOR . 'Module.php',
+return array(
+    'Application\View\Listener'              => __DIR__ . '/src/Application/View/Listener.php',
+    'Application\Controller\IndexController' => __DIR__ . '/src/Application/Controller/IndexController.php',
+    'Application\Controller\ErrorController' => __DIR__ . '/src/Application/Controller/ErrorController.php',
+    'Application\Module'                     => __DIR__ . '/Module.php',
 );
