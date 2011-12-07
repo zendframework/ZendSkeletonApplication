a4621753e75915d1b59f6d9793d57dbb443518f4
Evan Coury <me@evancoury.com>
2011-12-07 14:22:38 -0700

    Remove references to application environment.
    - Updated ZF2 submodule
    - Added local dist config file, gitignored non-dist counterpart
    - Added default global config file
    - Some comments explaining the purpose of the files

--- config/application.config.php
+++ config/application.config.php
@@ -8,8 +8,7 @@ return array(
         'Application',
     ),
     'module_listener_options' => array( 
-        'config_cache_enabled'    => false,
-        'cache_dir'               => realpath(dirname(__DIR__) . '/data/cache'),
-        'application_environment' => getenv('APPLICATION_ENV'),
+        'config_cache_enabled' => false,
+        'cache_dir'            => realpath(dirname(__DIR__) . '/data/cache'),
     ),
 );
--- module/Application/Module.php
+++ module/Application/Module.php
@@ -31,7 +31,7 @@ class Module implements AutoloaderProvider
         );
     }
 
-    public function getConfig($env = null)
+    public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
     }
