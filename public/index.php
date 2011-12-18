491bd52235289a2eeddc865797b338b95e6269be
Evan Coury <me@evancoury.com>
2011-12-18 07:54:05 -0700

    Check for ZF2_PATH env var when including ZF2 AutoloaderFactory

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 chdir(dirname(__DIR__));
-require_once 'vendor/ZendFramework/library/Zend/Loader/AutoloaderFactory.php';
+require_once (getenv('ZF2_PATH') ?: 'vendor/ZendFramework/library') . '/Zend/Loader/AutoloaderFactory.php';
 Zend\Loader\AutoloaderFactory::factory(array('Zend\Loader\StandardAutoloader' => array()));
 
 $appConfig = include 'config/application.config.php';
