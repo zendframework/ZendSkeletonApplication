97dc696bc9e4db85ae906e1cc71c8dfd9d7c04a7
YonmaN <yonmanm@gmail.com>
2012-03-29 16:57:52 +0300

    Fix for ZF2-243 requires we remove dependency in glob_brace and its syntax. Replaced the single pattern with multiple config listeners that collect global and local named configuration files Pull request for ZF2 repository: https://github.com/zendframework/zf2/pull/980

--- public/index.php
+++ public/index.php
@@ -7,7 +7,8 @@ $appConfig = include 'config/application.config.php';
 
 $listenerOptions  = new Zend\Module\Listener\ListenerOptions($appConfig['module_listener_options']);
 $defaultListeners = new Zend\Module\Listener\DefaultListenerAggregate($listenerOptions);
-$defaultListeners->getConfigListener()->addConfigGlobPath("config/autoload/{,*.}{global,local}.config.php");
+$defaultListeners->getConfigListener()->addConfigGlobPath("config/autoload/*local.config.php");
+$defaultListeners->getConfigListener()->addConfigGlobPath("config/autoload/*global.config.php");
     
 
 $moduleManager = new Zend\Module\Manager($appConfig['modules']);
