b2745188a1e20b47a37350018dd2bf7c0d14c3c5
Matthew Weier O'Phinney <matthew@zend.com>
2012-05-15 13:36:02 -0500

    [zen-33] Update to add doctype configuration

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -34,6 +34,7 @@ return array(
     'view_manager' => array(
         'display_not_found_reason' => true,
         'display_exceptions'       => true,
+        'doctype'                  => 'HTML5',
         'not_found_template'       => 'error/404',
         'exception_template'       => 'error/index',
         'template_map' => array(
