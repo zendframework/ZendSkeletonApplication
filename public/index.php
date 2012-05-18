c994772feb6fe02f60e7b875be37de9c64415882
Kyle Spraggs <theman@spiffyjr.me>
2012-05-18 09:26:34 -0500

    Added composer.lock to git. Added vendor/ to .gitignore, this directory is created by composer. Modified index.php to skip the autoloader factory.

--- public/index.php
+++ public/index.php
@@ -1,15 +1,12 @@
 <?php
 chdir(dirname(__DIR__));
 require_once('vendor/autoload.php');
-Zend\Loader\AutoloaderFactory::factory();
 
-$appConfig = include 'config/application.config.php';
-
+$appConfig        = include 'config/application.config.php';
 $sharedEvents     = new Zend\EventManager\SharedEventManager();
 $listenerOptions  = new Zend\Module\Listener\ListenerOptions($appConfig['module_listener_options']);
 $defaultListeners = new Zend\Module\Listener\DefaultListenerAggregate($listenerOptions);
 $defaultListeners->getConfigListener()->addConfigGlobPath("config/autoload/*.php");
-    
 
 $moduleManager = new Zend\Module\Manager($appConfig['modules']);
 $events        = $moduleManager->events();
