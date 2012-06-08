e9734534c5ac41e36317a2cbd30a1c924d86070f
Jan Pieper <kontakt@jan-pieper.info>
2012-06-08 16:20:22 +0300

    [zendframework/ZendSkeletonApplication#67] fixed path to index.phtml (was introduced by: 87bd76e8)

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -40,10 +40,10 @@ return array(
         'not_found_template'       => 'error/404',
         'exception_template'       => 'error/index',
         'template_map' => array(
-            'layout/layout' => __DIR__ . '/../view/layout/layout.phtml',
-            'index/index'   => __DIR__ . '/../view/index/index.phtml',
-            'error/404'     => __DIR__ . '/../view/error/404.phtml',
-            'error/index'   => __DIR__ . '/../view/error/index.phtml',
+            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+            'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
+            'error/404'               => __DIR__ . '/../view/error/404.phtml',
+            'error/index'             => __DIR__ . '/../view/error/index.phtml',
         ),
         'template_path_stack' => array(
             __DIR__ . '/../view',
