36a6cbc066c8f3a25defd744a40deffcaf403b9e
Matthew Weier O'Phinney <matthew@zend.com>
2012-07-02 06:35:25 -0500

    [zen-24] Removed the default route
    - Removed the default route, as it is not recommended.
    - See https://github.com/zendframework/ZendSkeletonModule/pull/3 for how
      a module-specific default route can be created.

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -2,20 +2,6 @@
 return array(
     'router' => array(
         'routes' => array(
-            'default' => array(
-                'type'    => 'Zend\Mvc\Router\Http\Segment',
-                'options' => array(
-                    'route'    => '/[:controller[/:action]]',
-                    'constraints' => array(
-                        'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                        'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
-                    ),
-                    'defaults' => array(
-                        'controller' => 'index',
-                        'action'     => 'index',
-                    ),
-                ),
-            ),
             'home' => array(
                 'type' => 'Zend\Mvc\Router\Http\Literal',
                 'options' => array(
