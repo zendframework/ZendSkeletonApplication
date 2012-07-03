4261f12838b8ac6140ecd483e873b3a79a4d1b91
Evan Coury <me@evancoury.com>
2012-07-03 15:12:38 -0700

    Clean up index.php significantly

--- public/index.php
+++ public/index.php
@@ -1,40 +1,12 @@
 <?php
-use Zend\Loader\AutoloaderFactory;
-use Zend\ServiceManager\ServiceManager;
-use Zend\Mvc\Service\ServiceManagerConfiguration;
-
+/**
+ * This makes our life easier when dealing with paths. Everything is relative
+ * to the application root now.
+ */
 chdir(dirname(__DIR__));
 
-// Composer autoloading
-if (file_exists('vendor/autoload.php')) {
-    $loader = include 'vendor/autoload.php';
-}
+// Setup autoloading
+include 'init_autoloader.php';
 
-// Support for ZF2_PATH environment variable or git submodule
-if ($zf2Path = getenv('ZF2_PATH') ?: (is_dir('vendor/ZF2/library') ? 'vendor/ZF2/library' : false)) {
-    if (isset($loader)) {
-        $loader->add('Zend', $zf2Path . '/Zend');
-    } else {
-        include $zf2Path . '/Zend/Loader/AutoloaderFactory.php';
-        AutoloaderFactory::factory(array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'autoregister_zf' => true
-            )
-        ));
-    }
-}
-
-if (!class_exists('Zend\Loader\AutoloaderFactory')) {
-    throw new RuntimeException('Unable to load ZF2. Run `php composer.phar install` or define a ZF2_PATH environment variable.');
-}
-
-// Get application stack configuration
-$configuration = include 'config/application.config.php';
-
-// Setup service manager
-$serviceManager = new ServiceManager(new ServiceManagerConfiguration($configuration['service_manager']));
-$serviceManager->setService('ApplicationConfiguration', $configuration);
-$serviceManager->get('ModuleManager')->loadModules();
-
-// Run application
-$serviceManager->get('Application')->bootstrap()->run()->send();
+// Run the application!
+Zend\Mvc\Application::init(include 'config/application.config.php')->run()->send();
