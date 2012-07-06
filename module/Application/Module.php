88f96187c3782f2359ff9f557b64549d63a360dc
Evan Coury <me@evancoury.com>
2012-07-05 19:40:41 -0700

    Initial translation setup

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -8,6 +8,7 @@ class Module
 {
     public function onBootstrap($e)
     {
+        $e->getApplication()->getServiceManager()->get('translator');
         $eventManager        = $e->getApplication()->getEventManager();
         $moduleRouteListener = new ModuleRouteListener();
         $moduleRouteListener->attach($eventManager);
