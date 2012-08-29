8a9326508c941a2b310d49f89e92497e96916268
Sammie S. Taunton <diemuzi@gmail.com>
2012-08-29 12:13:00 -0500

    Update module/Application/config/module.config.php
    Updated Translation Options

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -59,7 +59,7 @@ return array(
     ),
     'translator' => array(
         'locale' => 'en_US',
-        'translation_patterns' => array(
+        'translation_file_patterns' => array(
             array(
                 'type'     => 'gettext',
                 'base_dir' => __DIR__ . '/../language',
