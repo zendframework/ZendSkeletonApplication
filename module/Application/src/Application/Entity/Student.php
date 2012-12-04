41bfe7629ee648a91bf4af4a5cbbe084660766e4
Emilio Jimenez <emiliojimenez90@gmail.com>
2012-12-04 16:51:48 -0600

    updated student, student controller

--- module/Application/src/Application/Entity/Student.php
+++ module/Application/src/Application/Entity/Student.php
@@ -2,6 +2,7 @@
 
 namespace Application\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -17,6 +18,14 @@ class Student extends Person
      * @ORM\JoinColumn(name="major", referencedColumnName="id")
      */
     protected $major;
+    
+    /**
+     * @var ArrayCollection
+     * 
+     * @ORM\ManyToMany(targetEntity="Course", inversedBy="students")
+     * @ORM\JoinTable(name="students_courses")
+     */
+    protected $courses;
 
     /**
      * @param Major $major
@@ -31,6 +40,41 @@ class Student extends Person
      */
     public function getMajor()
     {
-        return $this->major;
+        return $this->major;   
+    }
+    
+    /**
+     * Adds a course to this student instance
+     */
+    public function addCourse($aCourse)
+    {
+        $this->courses->add($aCourse);
+                
+    }
+    
+    /**
+     * Removes a course from this student instance.
+     */
+    public function removeCourse()
+    {
+        
+    }
+    
+    /**
+     * 
+     * @param type $courses
+     */
+    public function setCourses($courses)
+    {
+        $this->courses = $courses;
+    }
+    
+    /**
+     * 
+     * @return ArrayCollection
+     */
+    public function getCourses()
+    {
+        return $this->courses;
     }
 }
