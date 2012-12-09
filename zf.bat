5d6a5ca92dfaed4848ddb88cafd28090073b2d81
Artur Bodera <abodera@gmail.com>
2012-12-09 15:00:07 +0100

    Add basic console support.

---
+++ zf.bat
@@ -0,0 +1,5 @@
+@echo off
+rem ---------------------------------------------------------------------------------------------------------------
+rem  Source: http://www.microsoft.com/resources/documentation/windows/xp/all/proddocs/en-us/percent.mspx?mfr=true
+rem ---------------------------------------------------------------------------------------------------------------
+php %~dp0\public\index.php %*
