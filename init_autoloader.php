5973503564af5c36e31f7bdf19608536df68d471
Matthew Weier O'Phinney <matthew@zend.com>
2013-03-04 08:26:31 -0600

    Alternate precedence order for ZF2 path
    - It makes more sense to assume that if ZF2 is actually _in_ the
      project, to use it, and only if not present start looking at the ENV
      and php.ini settings.

--- init_autoloader.php
+++ init_autoloader.php
@@ -23,12 +23,12 @@ if (file_exists('vendor/autoload.php')) {
 
 $zf2Path = false;
 
-if (getenv('ZF2_PATH')) {           // Support for ZF2_PATH environment variable or git submodule
+if (is_dir('vendor/ZF2/library')) {
+    $zf2Path = 'vendor/ZF2/library';
+} elseif (getenv('ZF2_PATH')) {      // Support for ZF2_PATH environment variable or git submodule
     $zf2Path = getenv('ZF2_PATH');
 } elseif (get_cfg_var('zf2_path')) { // Support for zf2_path directive value
     $zf2Path = get_cfg_var('zf2_path');
-} elseif (is_dir('vendor/ZF2/library')) {
-    $zf2Path = 'vendor/ZF2/library';
 }
 
 if ($zf2Path) {
