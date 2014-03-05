65b2152ed45cd24240d2351baba557fb954e9f52
Diego Sapriza <diego@sapriza.net>
2014-03-05 15:34:28 -0200

    zfcuser interface

--- module/CASEBase/src/CASEBase/Entity/User.php
+++ module/CASEBase/src/CASEBase/Entity/User.php
@@ -2,51 +2,145 @@
 namespace CASEBase\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Doctrine\Common\Collections\ArrayCollection;
+use ZfcUser\Entity\UserInterface;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="users")
  */
-class User
+class User implements UserInterface
 {
-    /**
-     * @ORM\Id @ORM\Column(type="integer")
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    private $id;
 
     /**
-     * @ORM\Column(type="string", unique=true))
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @var int
      */
-    private $name;
+    protected $id;
+
+    /**
+     *
+     * @var string @ORM\Column(type="string", unique=true, nullable=true)
+     */
+    protected $username;
 
     /**
      * @ORM\Column(type="string", unique=true, nullable=false)
      */
-    private $email;
+    protected $email;
 
     /**
      * @ORM\Column(type="string", nullable=true)
      */
-    private $display_name;
-    
+    protected $display_name;
+
     /**
      * @ORM\Column(type="string")
      */
-    private $password;
-    
+    protected $password;
+
     /**
-     * @ORM\Column(type="smallint")
+     * @ORM\Column(type="smallint", options={"default" = 0})
      */
-    private $state;
+    protected $state = 0;
+	/**
+     * @return the $id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
 
-    public function __construct(){
+	/**
+     * @param number $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
 
-    	
+	/**
+     * @return the $username
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+	/**
+     * @param string $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+	/**
+     * @return the $email
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+	/**
+     * @param field_type $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+	/**
+     * @return the $display_name
+     */
+    public function getDisplayName()
+    {
+        return $this->display_name;
+    }
+
+	/**
+     * @param field_type $display_name
+     */
+    public function setDisplayName($display_name)
+    {
+        $this->display_name = $display_name;
+    }
+
+	/**
+     * @return the $password
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+	/**
+     * @param field_type $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+	/**
+     * @return the $state
+     */
+    public function getState()
+    {
+        return $this->state;
+    }
+
+	/**
+     * @param field_type $state
+     */
+    public function setState($state)
+    {
+        $this->state = $state;
     }
 
 
-	public function __toString(){
-	}
+    
+    
 }
