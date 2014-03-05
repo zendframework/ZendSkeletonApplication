fc59b6a560b02122407479b5698ed8ac016fb4f2
Diego Sapriza <diego@sapriza.net>
2014-03-05 13:56:45 -0200

    doctrine config

--- config/application.config.php
+++ config/application.config.php
@@ -3,6 +3,9 @@ return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
         'Application',
+        'DoctrineModule',
+        'DoctrineORMModule',
+    	'CASEBase',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
