889880ca3ad81adc894cc3f9ed2b9919dab7a959
Abdul Malik Ikhsan <samsonasik@gmail.com>
2014-03-18 21:19:35 +0700

    move !class_exists(ZendLoaderAutoloaderFactory) before $zf2Path check based on #248 based on #248 , I think the class_exist for Zend\Loader\AutoloaderFactory check can be moved before vendor/ZF2/library check

--- init_autoloader.php
+++ init_autoloader.php
@@ -21,27 +21,29 @@ if (file_exists('vendor/autoload.php')) {
     $loader = include 'vendor/autoload.php';
 }
 
-$zf2Path = false;
-
-if (is_dir('vendor/ZF2/library')) {
-    $zf2Path = 'vendor/ZF2/library';
-} elseif (getenv('ZF2_PATH')) {      // Support for ZF2_PATH environment variable or git submodule
-    $zf2Path = getenv('ZF2_PATH');
-} elseif (get_cfg_var('zf2_path')) { // Support for zf2_path directive value
-    $zf2Path = get_cfg_var('zf2_path');
-}
-
-if ($zf2Path && !class_exists('Zend\Loader\AutoloaderFactory')) {
-    if (isset($loader)) {
-        $loader->add('Zend', $zf2Path);
-        $loader->add('ZendXml', $zf2Path);
-    } else {
-        include $zf2Path . '/Zend/Loader/AutoloaderFactory.php';
-        Zend\Loader\AutoloaderFactory::factory(array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'autoregister_zf' => true
-            )
-        ));
+if (!class_exists('Zend\Loader\AutoloaderFactory')) {
+    $zf2Path = false;
+    
+    if (is_dir('vendor/ZF2/library')) {
+        $zf2Path = 'vendor/ZF2/library';
+    } elseif (getenv('ZF2_PATH')) {      // Support for ZF2_PATH environment variable or git submodule
+        $zf2Path = getenv('ZF2_PATH');
+    } elseif (get_cfg_var('zf2_path')) { // Support for zf2_path directive value
+        $zf2Path = get_cfg_var('zf2_path');
+    }
+    
+    if ($zf2Path) {
+        if (isset($loader)) {
+            $loader->add('Zend', $zf2Path);
+            $loader->add('ZendXml', $zf2Path);
+        } else {
+            include $zf2Path . '/Zend/Loader/AutoloaderFactory.php';
+            Zend\Loader\AutoloaderFactory::factory(array(
+                'Zend\Loader\StandardAutoloader' => array(
+                    'autoregister_zf' => true
+                )
+            ));
+        }
     }
 }
 
