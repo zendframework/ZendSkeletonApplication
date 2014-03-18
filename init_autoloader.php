62ade7c5b05a5b5a648bbd8a3a2aaf04508ed20a
Abdul Malik Ikhsan <samsonasik@gmail.com>
2014-03-18 22:37:17 +0700

    remove unused new line

--- init_autoloader.php
+++ init_autoloader.php
@@ -49,7 +49,6 @@ if ($zf2Path) {
     }
 }
 
-
 if (!class_exists('Zend\Loader\AutoloaderFactory')) {
     throw new RuntimeException('Unable to load ZF2. Run `php composer.phar install` or define a ZF2_PATH environment variable.');
 }
