50db8ad553b1d3c062fe352f1faff3e669d0cf86
Diego Sapriza <diego@sapriza.net>
2014-03-24 18:33:49 -0300

    log

--- module/CASELog/README.md
+++ module/CASELog/README.md
@@ -1,1 +1,12 @@
 # CASE Log Module
+
+## Dependencies
+  -  https://github.com/enlitepro/enlite-monolog
+  
+```
+{
+    "require": {
+        "enlitepro/enlite-monolog": "~1.1.0"
+    }
+}
+```
--- module/CASELog/config/module.config.php
+++ module/CASELog/config/module.config.php
@@ -1,55 +1,22 @@
 <?php
 return array(
-    'controllers' => array(
-        'invokables' => array(
-            'CASELog\Controller\Log' => 'CASELog\Controller\LogController',
-        ),
-    ),
-    'router' => array(
-        'routes' => array(
-            'case-log' => array(
-                'type'    => 'Literal',
-                'options' => array(
-                    'route'    => '/log',
-                    'defaults' => array(
-                        '__NAMESPACE__' => 'CASELog\Controller',
-                        'controller'    => 'Log',
-                        'action'        => 'index',
-                    ),
-                ),
-                'may_terminate' => true,
-                'child_routes' => array(
-                    'default' => array(
-                        'type'    => 'Segment',
-                        'options' => array(
-                            'route'    => '/[:controller[/:action]]',
-                            'constraints' => array(
-                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
-                            ),
-                            'defaults' => array(
-                            ),
-                        ),
-                    ),
-                ),
-            ),
-        ),
-    ),
-    'view_manager' => array(
-        'template_path_stack' => array(
-            'CASELog' => __DIR__ . '/../view',
-        ),
-    ),
-    'bjyauthorize' => array(
-        'guards' => array(
-            'BjyAuthorize\Guard\Route' => array(
-                array(
-                    'route' => 'case-log',
-                    'roles' => array(
-                        'administrator',
+    'EnliteMonolog' => array(
+        // Logger name
+        'EnliteMonologService' => array(
+            // name of
+            'name' => 'default',
+            // Handlers, it can be service manager alias(string) or config(array)
+            'handlers' => array(
+                'default' => array(
+                    'name' => 'Monolog\Handler\StreamHandler',
+                    'args' => array(
+                        //'path' => 'data/log/application.log',
+                        'path' => '/tmp/application.log',
+                        'level' => \Monolog\Logger::DEBUG,
+                        'bubble' => true
                     )
-                ),
+                )
             )
         )
-    )
+    ),    
 );
