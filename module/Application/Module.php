15a4869becab1e481f1201bb5405f4b2b62233f4
Jesus Vieira <jesusvieiradelima@gmail.com>
2014-04-30 23:44:00 -0400

    implement interfaces in module.php #260

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -9,23 +9,44 @@
 
 namespace Application;
 
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface,
+    Zend\ModuleManager\Feature\BootstrapListenerInterface,
+    Zend\ModuleManager\Feature\ConfigProviderInterface,
+    Zend\EventManager\EventInterface;
 use Zend\Mvc\ModuleRouteListener;
-use Zend\Mvc\MvcEvent;
 
-class Module
+
+class Module implements BootstrapListenerInterface, ConfigProviderInterface, AutoloaderProviderInterface
 {
-    public function onBootstrap(MvcEvent $e)
+    /**
+     * Listen to the bootstrap event
+     *
+     * @param EventInterface $e
+     *
+     * @return array
+     */
+    public function onBootstrap(EventInterface $e)
     {
         $eventManager        = $e->getApplication()->getEventManager();
         $moduleRouteListener = new ModuleRouteListener();
         $moduleRouteListener->attach($eventManager);
     }
 
+    /**
+     * Returns configuration to merge with application configuration
+     *
+     * @return array|\Traversable
+     */
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
     }
 
+    /**
+     * Return an array for passing to Zend\Loader\AutoloaderFactory.
+     *
+     * @return array
+     */
     public function getAutoloaderConfig()
     {
         return array(
