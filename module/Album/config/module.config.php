19cb463a29428f4c363b2e8db3b4002134bd254c
getJv <jhonatan.morais@PAT009518.agrario.gov.br>
2015-02-09 15:32:43 -0200

    atualização da rotas

--- module/Album/config/module.config.php
+++ module/Album/config/module.config.php
@@ -5,6 +5,26 @@ return array(
              'Album\Controller\Album' => 'Album\Controller\AlbumController',
          ),
      ),
+	      // The following section is new and should be added to your file
+     'router' => array(
+         'routes' => array(
+             'album' => array(
+                 'type'    => 'segment',
+                 'options' => array(
+                     'route'    => '/album[/:action][/:id]',
+                     'constraints' => array(
+                         'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                         'id'     => '[0-9]+',
+                     ),
+                     'defaults' => array(
+                         'controller' => 'Album\Controller\Album',
+                         'action'     => 'index',
+                     ),
+                 ),
+             ),
+         ),
+     ),
+
      'view_manager' => array(
          'template_path_stack' => array(
              'album' => __DIR__ . '/../view',
