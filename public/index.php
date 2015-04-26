7e7b696037b39f07d52038017810268133955f4b
Peter Bacinsky <peter@bacinsky.sk>
2015-04-26 05:11:51 +0200

    Updated public/index.php, exclude cli-server static file requests to self

--- public/index.php
+++ public/index.php
@@ -6,9 +6,11 @@
 chdir(dirname(__DIR__));
 
 // Decline static file requests back to the PHP built-in webserver
-if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
+$path = __DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+if (php_sapi_name() === 'cli-server' && __FILE__ !== $path && is_file($path)) {
     return false;
 }
+unset($path);
 
 // Setup autoloading
 require 'init_autoloader.php';
