eba4ea881828cb6a3443bd71b6f1f1a5fe4b0057
Peter Bacinsky <peter@bacinsky.sk>
2015-05-15 19:15:57 +0200

    Updated public/index.php, realpath() the $path to decide if it's a same file (when symlink)

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@
 chdir(dirname(__DIR__));
 
 // Decline static file requests back to the PHP built-in webserver
-$path = __DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$path = realpath(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 if (php_sapi_name() === 'cli-server' && __FILE__ !== $path && is_file($path)) {
     return false;
 }
