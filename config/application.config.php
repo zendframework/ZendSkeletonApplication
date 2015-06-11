81e99dbe6ae4ea98da9e9bcadb9077935512f753
Rob Allen <rob@akrabat.com>
2015-06-11 09:23:00 +0100

    Update links to config examples in manual to use latest version

--- config/application.config.php
+++ config/application.config.php
@@ -1,8 +1,9 @@
 <?php
 /**
- * If you need an environment-specific system configuration,
+ * If you need an environment-specific system or application configuration,
  * there is an example in the documentation
- * @see http://framework.zend.com/manual/2.3/en/tutorials/config.advanced.html#environment-specific-system-configuration
+ * @see http://framework.zend.com/manual/current/en/tutorials/config.advanced.html#environment-specific-system-configuration
+ * @see http://framework.zend.com/manual/current/en/tutorials/config.advanced.html#environment-specific-application-configuration
  */
 return array(
     // This should be an array of module namespaces used in the application.
