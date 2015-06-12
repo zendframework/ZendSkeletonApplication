4efd2865f308df64f741c30f7137005f42d24924
Rob Allen <rob@akrabat.com>
2015-06-12 08:10:15 +0100

    Only test for path when the SAPI is cli-server

--- public/index.php
+++ public/index.php
@@ -6,11 +6,13 @@
 chdir(dirname(__DIR__));
 
 // Decline static file requests back to the PHP built-in webserver
-$path = realpath(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
-if (php_sapi_name() === 'cli-server' && __FILE__ !== $path && is_file($path)) {
-    return false;
+if (php_sapi_name() === 'cli-server') {
+    $path = realpath(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+    if (__FILE__ !== $path && is_file($path)) {
+        return false;
+    }
+    unset($path);
 }
-unset($path);
 
 // Setup autoloading
 require 'init_autoloader.php';
