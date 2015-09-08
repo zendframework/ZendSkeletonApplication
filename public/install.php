fc9ac8e2fda445babb85b93bc2afe24c2167c043
Stanimir Dimitrov <stanimirdim92@gmail.com>
2015-09-08 16:50:11 +0300

    license fix, code fix

--- public/install.php
+++ public/install.php
@@ -1,34 +1,10 @@
 <?php
 /**
- * MIT License
- * ===========
+ * Zend Framework (http://framework.zend.com/)
  *
- * Copyright (c) 2015 Stanimir Dimitrov <stanimirdim92@gmail.com>
- *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
- *
- * The above copyright notice and this permission notice shall be included
- * in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
- * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
- * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
- * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- * @author     Stanimir Dimitrov <stanimirdim92@gmail.com>
- * @copyright  2015 Stanimir Dimitrov.
- * @license    http://www.opensource.org/licenses/mit-license.php  MIT License
- * @version    0.0.11
- * @link       TBA
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
 use Symfony\Component\Process\Process;
@@ -63,31 +39,6 @@ chdir(dirname(__DIR__));
 
 <?php
 $pageId = (isset($_GET["page"]) ? $_GET["page"] : 0);
-
-/**
- * These directories are required
- */
-$requiredDirs = [
-    'data',
-    'data/cache',
-    'data/cache/frontend',
-    'data/cache/modules',
-    'data/logs',
-    'data/translations',
-    'data/fonts',
-    'data/database',
-    'public/userfiles/captcha',
-    'public/userfiles/exports',
-    'public/userfiles/images',
-    'public/userfiles/preview',
-];
-
-foreach ($requiredDirs as $dir) {
-    if (!is_dir($dir)) {
-        mkdir($dir, 0750, true);
-    }
-    chmod($dir, 0750);
-}
 
 switch ((int) $pageId) {
     case 1:
