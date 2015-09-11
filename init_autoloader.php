af86755edbe49ebfc61ee0dcaa17de733fa5d98a
Abdul Malik Ikhsan <samsonasik@gmail.com>
2015-09-11 14:26:12 +0700

    remove ZF2_PATH env usage as requirement is zf ~2.5

--- init_autoloader.php
+++ init_autoloader.php
@@ -25,28 +25,6 @@ if (class_exists('Zend\Loader\AutoloaderFactory')) {
     return;
 }
 
-$zf2Path = false;
-
-if (getenv('ZF2_PATH')) {            // Support for ZF2_PATH environment variable
-    $zf2Path = getenv('ZF2_PATH');
-} elseif (get_cfg_var('zf2_path')) { // Support for zf2_path directive value
-    $zf2Path = get_cfg_var('zf2_path');
-}
-
-if ($zf2Path) {
-    if (isset($loader)) {
-        $loader->add('Zend', $zf2Path);
-        $loader->add('ZendXml', $zf2Path);
-    } else {
-        include $zf2Path . '/Zend/Loader/AutoloaderFactory.php';
-        Zend\Loader\AutoloaderFactory::factory(array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'autoregister_zf' => true
-            )
-        ));
-    }
-}
-
 if (!class_exists('Zend\Loader\AutoloaderFactory')) {
     throw new RuntimeException('Unable to load ZF2. Run `php composer.phar install` or define a ZF2_PATH environment variable.');
 }
