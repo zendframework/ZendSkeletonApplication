1b7d6e6e6192b89e946a19f41d68b4dc313e2938
Neftalí Yagua <neftali.yagua@gmail.com>
2015-10-08 15:04:33 -0430

    Update index.php

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
  /**
   * Display all errors when APPLICATION_ENV is development.
   */
-if (getenv('APPLICATION_ENV') && getenv('APPLICATION_ENV') == 'development') {
+if (getenv('APPLICATION_ENV') == 'development') {
      error_reporting(E_ALL);
      ini_set("display_errors", 1);
 }
