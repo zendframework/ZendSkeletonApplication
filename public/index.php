908b6f9315507ecffac3183bb7771e2d4d5dcaf4
Gianluca Arbezzano <gianarb92@gmail.com>
2015-10-19 20:29:35 +0200

    Fixed ENV Variables and replace expection message

--- public/index.php
+++ public/index.php
@@ -5,13 +5,23 @@
  */
 chdir(dirname(__DIR__));
 
+// Decline static file requests back to the PHP built-in webserver
+if (php_sapi_name() === 'cli-server') {
+    $path = realpath(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+    if (__FILE__ !== $path && is_file($path)) {
+        return false;
+    }
+    unset($path);
+}
+
 // Composer autoloading
 if (!file_exists('vendor/autoload.php')) {
     throw new RuntimeException(
-        'Unable to load ZF2. Run `php composer.phar install` or define a ZF2_PATH environment variable.'
+        'Unable to load ZF2. Run `php composer.phar install`.'
     );
 }
-$loader = include 'vendor/autoload.php';
+
+require 'vendor/autoload.php';
 
 // Run the application!
 Zend\Mvc\Application::init(require 'config/application.config.php')->run();
