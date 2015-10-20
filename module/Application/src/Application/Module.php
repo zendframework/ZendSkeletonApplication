da440ddeea5ca5176b882b56ec2128c01d14e457
Gianluca Arbezzano <gianarb92@gmail.com>
2015-10-20 11:50:23 +0200

    Migrate to psr4 and move Module.php

--- module/Application/src/Application/Module.php
+++ module/Application/src/Application/Module.php
@@ -23,6 +23,6 @@ class Module
 
     public function getConfig()
     {
-        return include __DIR__ . '/config/module.config.php';
+        return include __DIR__ . '/../../config/module.config.php';
     }
 }
