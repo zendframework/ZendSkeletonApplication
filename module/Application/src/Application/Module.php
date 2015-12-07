35053e070374658c7b45c47d67149d266cc8c601
Gianluca Arbezzano <gianarb92@gmail.com>
2015-12-07 21:52:30 +0000

    Replace include module config with require

--- module/Application/src/Application/Module.php
+++ module/Application/src/Application/Module.php
@@ -23,6 +23,6 @@ class Module
 
     public function getConfig()
     {
-        return include __DIR__ . '/../../config/module.config.php';
+        return require __DIR__ . '/../../config/module.config.php';
     }
 }
