28ba4f482de3baf63a8f9bda63884b7b3c4f3df6
Matthew Weier O'Phinney <matthew@zend.com>
2016-05-25 17:17:53 -0500

    Use absolute paths within bootstrap script

--- public/index.php
+++ public/index.php
@@ -18,11 +18,11 @@ if (php_sapi_name() === 'cli-server') {
 }
 
 // Composer autoloading
-include 'vendor/autoload.php';
+include __DIR__ . '/../vendor/autoload.php';
 
 if (! class_exists(Application::class)) {
     throw new RuntimeException('Unable to load ZF2. Run `php composer.phar install`.');
 }
 
 // Run the application!
-Application::init(require 'config/application.config.php')->run();
+Application::init(require __DIR__ . '/../config/application.config.php')->run();
