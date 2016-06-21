1800418b4170f1ce7b9cd39eccdf1331de4ec5b0
Matthew Weier O'Phinney <matthew@zend.com>
2016-06-21 17:43:34 -0500

    Fix path in bootstrap script

--- public/index.php
+++ public/index.php
@@ -32,7 +32,7 @@ if (! class_exists(Application::class)) {
 
 // Retrieve configuration
 $appConfig = require __DIR__ . '/../config/application.config.php';
-if (file_exists(__DIR__ . '/config/development.config.php')) {
+if (file_exists(__DIR__ . '/../config/development.config.php')) {
     $appConfig = ArrayUtils::merge($appConfig, require __DIR__ . '/../config/development.config.php');
 }
 
