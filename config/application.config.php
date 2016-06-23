bec15ed5139fa455fd2d4e8dcf481bad8d6a9703
Matthew Weier O'Phinney <matthew@zend.com>
2016-06-23 09:15:05 -0500

    Provide production settings out-of-the-box
    - Enable config caching, and provide a cache key
    - Enable module map caching, and provide a cache key
    - Specify the configuration cache directory
    - Ensure glob paths are fully qualified (required for IBM i Series)

--- config/application.config.php
+++ config/application.config.php
@@ -24,27 +24,27 @@ return [
         // modules are loaded. These effectively override configuration
         // provided by modules themselves. Paths may use GLOB_BRACE notation.
         'config_glob_paths' => [
-            'config/autoload/{{,*.}global,{,*.}local}.php',
+            realpath(__DIR__) . '/autoload/{{,*.}global,{,*.}local}.php',
         ],
 
         // Whether or not to enable a configuration cache.
         // If enabled, the merged configuration will be cached and used in
         // subsequent requests.
-        //'config_cache_enabled' => $booleanValue,
+        'config_cache_enabled' => true,
 
         // The key used to create the configuration cache file name.
-        //'config_cache_key' => $stringKey,
+        'config_cache_key' => 'application.config.cache',
 
         // Whether or not to enable a module class map cache.
         // If enabled, creates a module class map cache which will be used
         // by in future requests, to reduce the autoloading process.
-        //'module_map_cache_enabled' => $booleanValue,
+        'module_map_cache_enabled' => true,
 
         // The key used to create the class map cache file name.
-        //'module_map_cache_key' => $stringKey,
+        'module_map_cache_key' => 'application.module.cache',
 
         // The path in which to cache merged configuration.
-        //'cache_dir' => $stringPath,
+        'cache_dir' => 'data/cache/',
 
         // Whether or not to enable modules dependency checking.
         // Enabled by default, prevents usage of modules that depend on other modules
@@ -53,16 +53,16 @@ return [
     ],
 
     // Used to create an own service manager. May contain one or more child arrays.
-    //'service_listener_options' => array(
-    //     array(
+    //'service_listener_options' => [
+    //     [
     //         'service_manager' => $stringServiceManagerName,
     //         'config_key'      => $stringConfigKey,
     //         'interface'       => $stringOptionalInterface,
     //         'method'          => $stringRequiredMethodName,
-    //     ),
-    // ),
+    //     ],
+    // ],
 
    // Initial configuration with which to seed the ServiceManager.
    // Should be compatible with Zend\ServiceManager\Config.
-   // 'service_manager' => array(),
+   // 'service_manager' => [],
 ];
