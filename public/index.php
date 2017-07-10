aee34b44255aa5cd5b30b3d98190bbb2215e3f72
jumaxbrian <jumaxbrian@gmail.com>
2017-07-10 11:28:47 +0300

    initial commit

--- public/index.php
+++ public/index.php
@@ -1,4 +1,17 @@
 <?php
+
+use Zend\Mvc\Application;
+use Zend\Stdlib\ArrayUtils;
+
+
+ /**
+  * Display all errors when APPLICATION_ENV is development.
+  */
+ if ($_SERVER['APPLICATION_ENV'] == 'development') {
+     error_reporting(E_ALL);
+     ini_set("display_errors", 1);
+ }
+ 
 /**
  * This makes our life easier when dealing with paths. Everything is relative
  * to the application root now.
@@ -6,12 +19,31 @@
 chdir(dirname(__DIR__));
 
 // Decline static file requests back to the PHP built-in webserver
-if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
-    return false;
+if (php_sapi_name() === 'cli-server') {
+    $path = realpath(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+    if (__FILE__ !== $path && is_file($path)) {
+        return false;
+    }
+    unset($path);
 }
 
-// Setup autoloading
-require 'init_autoloader.php';
+// Composer autoloading
+include __DIR__ . '/../vendor/autoload.php';
+
+if (! class_exists(Application::class)) {
+    throw new RuntimeException(
+        "Unable to load application.\n"
+        . "- Type `composer install` if you are developing locally.\n"
+        . "- Type `vagrant ssh -c 'composer install'` if you are using Vagrant.\n"
+        . "- Type `docker-compose run zf composer install` if you are using Docker.\n"
+    );
+}
+
+// Retrieve configuration
+$appConfig = require __DIR__ . '/../config/application.config.php';
+if (file_exists(__DIR__ . '/../config/development.config.php')) {
+    $appConfig = ArrayUtils::merge($appConfig, require __DIR__ . '/../config/development.config.php');
+}
 
 // Run the application!
-Zend\Mvc\Application::init(require 'config/application.config.php')->run();
+Application::init($appConfig)->run();
