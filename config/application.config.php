87fb8194ee4d16cf84feb3cfdfd49fc10e1907f7
Abdul Malik Ikhsan <samsonasik@gmail.com>
2018-03-18 03:31:27 +0700

    remove module_paths settings as using use_zend_loader = false

--- config/application.config.php
+++ config/application.config.php
@@ -15,15 +15,6 @@ return [
          // use composer autoloader instead of zend-loader
         'use_zend_loader' => false,
         
-        // This should be an array of paths in which modules reside.
-        // If a string key is provided, the listener will consider that a module
-        // namespace, the value of that key the specific path to that module's
-        // Module class.
-        'module_paths' => [
-            './module',
-            './vendor',
-        ],
-
         // An array of paths from which to glob configuration files after
         // modules are loaded. These effectively override configuration
         // provided by modules themselves. Paths may use GLOB_BRACE notation.
